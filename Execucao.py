b5608250f3931afba8a3d05073810c77ed5842d7
brunomaion <brunosmaion@hotmail.com>
2025-12-08 00:25:34 -0300

    graficos

--- Execucao.py
+++
@@ -1,7 +0,0 @@
-
-class Execucao:
-    def __init__(self, problema=None, algoritmo=None, tempo_execucao=0, melhor_solucao=None):
-        self.problema = problema
-        self.algoritmo = algoritmo
-        self.tempo_execucao = tempo_execucao
-        self.melhor_solucao = melhor_solucao
